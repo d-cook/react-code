5424c537f1e3cc94aa78ce47e667628e3d71a45b
Dan Cook <dcook0819@gmail.com>
2021-08-14 21:56:22 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+
+function FuncViewer() {
+
+}
+
+function FuncView() {
+
+}
+
+function StepsView() {
+
+}
+
+function RecordView() {
+
+}
+
+function ListView() {
+
+}
+
+function ValueView() {
+  return (
+    <input type="text" />
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
